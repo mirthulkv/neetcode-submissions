8048bd3322d7fb7a2d01e9d3863d368b607e8166
mirthulkv <kvmirthul@gmail.com>
2026-08-02 17:12:06 +0530

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.java
@@ -0,0 +1,26 @@
+class Solution {
+
+    public boolean isPalindrome(String s) {
+
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i < s.length(); i++) {
+
+            char ch = s.charAt(i);
+
+            if(Character.isLetterOrDigit(ch)) {
+                sb.append(Character.toLowerCase(ch));
+            }
+        }
+
+        String original = sb.toString();
+        String reversed = sb.reverse().toString();
+
+        return original.equals(reversed);
+    }
+}
+        
+        
+
+  
+
