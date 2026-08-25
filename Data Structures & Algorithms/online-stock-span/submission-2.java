090f8c9d0fe360849ae8486e032d4928893ff64e
mirthulkv <kvmirthul@gmail.com>
2026-08-25 15:59:24 +0530

    Add: online-stock-span - submission-2

---
+++ Data Structures & Algorithms/online-stock-span/submission-2.java
@@ -0,0 +1,25 @@
+class StockSpanner {
+
+    Stack<Integer> prices;
+    Stack<Integer> spans;
+
+    public StockSpanner() {
+        prices = new Stack<>();
+        spans = new Stack<>();
+    }
+
+    public int next(int price) {
+
+        int span = 1;
+
+        while (!prices.isEmpty() && prices.peek() <= price) {
+            prices.pop();
+            span += spans.pop();
+        }
+
+        prices.push(price);
+        spans.push(span);
+
+        return span;
+    }
+}
